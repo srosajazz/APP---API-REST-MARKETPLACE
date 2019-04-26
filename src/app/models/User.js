605796c37ede6c0a30ef73529fa16571394edff8
Sergio Rosa <scarlosjazz@gmail.com>
2019-04-26 13:33:08 -0400

    Password Cryptography

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -20,4 +21,11 @@ const UserSchema = new mongoose.Schema({
     default: Date.now
   }
 })
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next()
+  }
+  this.password = await bcrypt.hash(this.password, 8)
+})
+
 module.exports = mongoose.model('User', UserSchema)
